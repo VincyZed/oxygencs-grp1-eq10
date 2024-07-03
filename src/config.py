86ba0c685a53f213322597342f9ec9c9a98de082
Vincent Zauhar <v.zauhar@bell.net>
2024-07-03 18:50:39 -0400

    Set-up and fix Pylint errors

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
+"""Config file for Oxygen CS. Responsible for creating the database engine and session."""
+
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
